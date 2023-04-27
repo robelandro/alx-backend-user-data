9688d3ff7e417e8e141fb2c0b14e79abb6b53d91
robelpop <robelpopandro@gmail.com>
2023-04-27 20:34:22 -0200

    Solve: added mplement the format method to filter values in incoming log records

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,7 @@
 Filtered Logger module
 """
 import re
+import logging
 from typing import List
 
 
@@ -15,3 +16,22 @@ def filter_datum(fields: List[str], redaction: str, message: str,
         message = re.sub(fr'{f}=.+?{separator}', f'{f}={redaction}{separator}',
                          message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str] = None):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """filters incoming records using filter_datum
+        """
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
