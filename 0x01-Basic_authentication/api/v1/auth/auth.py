139758d9db75bae4813320df09e395c11aff9d0f
robelpop <robelpopandro@gmail.com>
2023-05-03 16:46:20 -0200

    Solve: class is the template for all authentication system

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+a module to manage the API authentication.
+"""
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    """a class to manage the API authentication.
+    """
+    
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        a method that returns False - path and excluded_paths will not be used.
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        a method that returns None - request will not be used.
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        a method that returns None - request will not be used.
+        """
+        return None
