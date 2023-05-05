ccfea0ced5db06f6b0317774df35f374e84da30d
robelpop <robelpopandro@gmail.com>
2023-05-05 21:06:18 -0200

    Solve: 5. Request validation

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+BasicAuth module for the API
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """A basic auth class to manage the API authentication"""
+
+    pass
