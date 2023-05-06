130f56294bda7a9ff93870d28db5d0b7f3cf8e6b
robelpop <robelpopandro@gmail.com>
2023-05-06 16:50:30 -0200

    solved: SessionAuth

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,11 @@
+#!/user/bin/env python3
+"""Session Authentication"""
+
+from api.v1.auth.auth import Auth
+from models.user import User
+
+
+class SessionAuth(Auth):
+    """Session Authentication Class
+    """
+    pass
