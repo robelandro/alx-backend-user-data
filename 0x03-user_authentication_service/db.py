1b6cd29344836c3c0c6737ee8321bacc8bd2c72e
robelpop <robelpopandro@gmail.com>
2023-05-12 00:03:43 -0200

    Solve: Find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -2,8 +2,10 @@
 """DB module.
 """
 from sqlalchemy import create_engine
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.orm.session import Session
 
 from user import Base, User
@@ -41,3 +43,13 @@ class DB:
             self._session.rollback()
             new_user = None
         return new_user
+    
+    def find_user_by(self, **kwargs) -> User:
+        """Finds a user instance in the database.
+        """
+        if not kwargs:
+            raise InvalidRequestError
+        search = self._session.query(User).filter_by(**kwargs).first()
+        if search is None:
+            raise NoResultFound
+        return search
